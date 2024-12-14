952b98f702a8cb1c41483187751079b0731f6169
Muhammed Sahad P <zanusahad@gmail.com>
2024-12-14 12:43:17 +0530

    feat(server):implement nodemailer for sending mail and forgot password and reset password controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -5,6 +5,7 @@ import User from "../models/userModel";
 import { CustomError } from "../utils/error/customError";
 import { RegisterSchema, LoginSchema } from "../utils/zodSchemas";
 import { StandardResponse } from "../utils/standardResponse";
+import mailSender from "../utils/mailSender";
 
 // Register a user
 const register = async (req: Request, res: Response) => {
@@ -13,6 +14,7 @@ const register = async (req: Request, res: Response) => {
   const saltRounds = parseInt(process.env.SALT_ROUNDS || "10", 10);
 
   const existingUser = await User.findOne({ email });
+
   if (existingUser) {
     throw new CustomError("User already exists", 400);
   }
@@ -34,7 +36,7 @@ const register = async (req: Request, res: Response) => {
 
   res
     .status(201)
-    .json(new StandardResponse("User created successfully", response));
+    .json(new StandardResponse("User registered successfully", response));
 };
 
 // Login a user
@@ -70,4 +72,78 @@ const login = async (req: Request, res: Response) => {
     .json(new StandardResponse("User logged in successfully", response));
 };
 
-export { register, login };
+//forgot password
+const forgotPassword = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  if (!email) {
+    throw new CustomError("Email is required", 400);
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError("User not found", 404);
+  }
+
+  const resetToken = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET_KEY || "",
+    {
+      expiresIn: "10m",
+    }
+  );
+
+  await mailSender(
+    email,
+    "Reset your password",
+    `
+	<div style="font-family: Arial, sans-serif; text-align: center;">
+	  <h2>Reset your password</h2>
+	  <p>Please click on the following link to reset your password:</p>
+	  <a href="${process.env.CLIENT_URL}/reset-password/${resetToken}">Reset password</a>
+	  <p>expires in 10 minutes</p>
+	  <p>Don't share this link with anyone!! </p>
+	  <p>Thank you!</p>
+	</div>
+	`
+  );
+
+  res.status(200).json(new StandardResponse("Email sent successfully"));
+};
+
+//reset password
+const resetPassword = async (req: Request, res: Response) => {
+  const token = req.params.token;
+
+  if (!token) {
+    throw new CustomError("Token is required", 400);
+  }
+
+  const { newPassword } = req.body;
+
+  if (!newPassword) {
+    throw new CustomError("New password is required", 400);
+  }
+
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY || "") as {
+    id: string;
+  };
+
+  const user = await User.findById(decodedToken.id);
+
+  if (!user) {
+    throw new CustomError("User not found", 404);
+  }
+
+  const saltRounds = parseInt(process.env.SALT_ROUNDS || "10", 10);
+  const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+  await user.updateOne({ password: hashedPassword });
+
+  res.status(200).json(new StandardResponse("Password reset successfully"));
+};
+
+export { register, login, forgotPassword, resetPassword };
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/authController";
 import { errorCatch } from "../utils/error/errorCatch";
 
 const router = express.Router();
 
 router.post("/register", errorCatch(register));
 router.post("/login", errorCatch(login));
+router.post("/forgotpassword", errorCatch(forgotPassword));
+router.patch("/resetpassword/:token", errorCatch(resetPassword));
 
 export default router;
