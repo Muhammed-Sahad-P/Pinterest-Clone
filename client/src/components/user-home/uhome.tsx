eef119c37a8b98c03837837a361d58be65b19551
Muhammed Sahad P <zanusahad@gmail.com>
2024-12-25 15:16:31 +0530

    feat(client): update home page and fetch pins

--- client/src/components/user-home/uhome.tsx
+++ client/src/components/user-home/uhome.tsx
@@ -1,15 +1,34 @@
+"use client";
+import { useEffect } from "react";
+import { AppDispatch, RootState } from "@/lib/store";
+import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
-import React from "react";
+import { fetchPins } from "@/lib/store/thunks/pin-thunk";
 
-export default function UserHome() {
+const UserHome = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const { pins, loading, error } = useSelector((state: RootState) => state.pin);
+
+    useEffect(() => {
+        dispatch(fetchPins());
+    }, [dispatch]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return (
         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
-            {Array.from({ length: 21 }, (_, index) => (
+            {pins?.data?.map((pin: any, index: number) => (
                 <div key={index} className="relative w-full aspect-w-1 aspect-h-1">
                     <Image
                         className="object-cover rounded-lg"
-                        src={`/${index + 1}.jpg`}
-                        alt={`Image ${index + 1}`}
+                        src={pin.imageUrl}
+                        alt={`Pin ${pin._id}`}
                         width={500}
                         height={300}
                     />
@@ -17,4 +36,28 @@ export default function UserHome() {
             ))}
         </div>
     );
-}
+};
+
+export default UserHome;
+
+
+// import Image from "next/image";
+// import React from "react";
+
+// export default function UserHome() {
+//     return (
+//         <div className="grid grid-cols-2 md:grid-cols-6 gap-4">
+//             {Array.from({ length: 21 }, (_, index) => (
+//                 <div key={index} className="relative w-full aspect-w-1 aspect-h-1">
+//                     <Image
+//                         className="object-cover rounded-lg"
+//                         src={`/${index + 1}.jpg`}
+//                         alt={`Image ${index + 1}`}
+//                         width={500}
+//                         height={300}
+//                     />
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
