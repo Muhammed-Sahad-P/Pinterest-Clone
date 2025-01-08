726f67a00a533f40e906ff3846bb6829270452e3
Muhammed Sahad P <zanusahad@gmail.com>
2025-01-08 14:11:08 +0530

    feat(server): update cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,7 @@ app.get("/", (_req, res) => {
   res.send("Hello world!");
 });
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
